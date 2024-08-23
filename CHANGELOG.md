b67c40d4d3953e454cb1c2cf9ffdcad8bb7fdd2c
hashxtree <145032760+hashxtree@users.noreply.github.com>
2024-08-23 16:59:11 +0300

    json decode/encode

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -35,10 +35,10 @@ Ref: https://keepachangelog.com/en/1.0.0/
 
 # Changelog
 
-## [v0.0.1] - 2024-08-23
+## [v0.0.2] - 2024-08-23
 
 ### Features
 
-...
+* (request) encode/decode json
 
 
---
+++ request/json.go
@@ -0,0 +1,30 @@
+package blockbook
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/pkg/errors"
+	"io"
+)
+
+func DecodeJSONResponse[T any](body io.Reader) (T, error) {
+	var res T
+
+	if body == nil {
+		return res, errors.New("no body to read")
+	}
+
+	if err := json.NewDecoder(body).Decode(&res); err != nil {
+		return res, errors.Wrap(err, "decode body")
+	}
+
+	return res, nil
+}
+
+func EncodeJSONRequest(request any) (io.Reader, error) {
+	encoded, err := json.Marshal(request)
+	if err != nil {
+		return nil, errors.Wrap(err, "marshal request")
+	}
+	return bytes.NewBuffer(encoded), nil
+}
