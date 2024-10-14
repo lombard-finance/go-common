32f6f15c42f21116f65de75d5069045a5940f577
Olivia Thet <oliviathet@gmail.com>
2024-10-14 11:54:51 -0400

    normalize url

---
+++ config/url.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"net/url"
+	"strings"
+)
+
+func NormalizeURL(address string) (*url.URL, error) {
+	// Trim any leading or trailing whitespace
+	address = strings.TrimSpace(address)
+
+	// Parse the URL
+	u, err := url.Parse(address)
+	if err != nil {
+		return nil, err
+	}
+
+	// If no scheme is specified, default to https
+	if u.Scheme == "" {
+		u.Scheme = "https"
+	} else {
+		// Force https if http was specified
+		u.Scheme = "https"
+	}
+
+	// Ensure there's a path
+	if u.Path == "" {
+		u.Path = "/"
+	}
+
+	// Remove username and password if present
+	u.User = nil
+
+	// Lowercase the host
+	u.Host = strings.ToLower(u.Host)
+
+	return u, nil
+}
