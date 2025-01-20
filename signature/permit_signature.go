502a7405836365256681ad9a3fe3ec54b30b9b4a
0xBECEDA <sakharovasonya1@gmail.com>
2025-01-20 18:42:30 +0100

    fix naming

--- signature/permit_signature.go
+++ signature/permit_signature.go
@@ -13,7 +13,7 @@ import (
 	"time"
 )
 
-type Permit struct {
+type StakeAndBakePermit struct {
 	Owner    common.MixedcaseAddress `json:"owner"`
 	Spender  common.MixedcaseAddress `json:"to"`
 	Value    math.Decimal256         `json:"value"`
@@ -24,15 +24,14 @@ type Permit struct {
 	ChainId math.HexOrDecimal256    `json:"chainId"`
 }
 
-func NewFromJson(jsonTypedData string) (*Permit, error) {
-
+func NewStakeAndBakePermitFromJson(jsonTypedData string) (*StakeAndBakePermit, error) {
 	var data apitypes.TypedData
 
 	if err := json.Unmarshal([]byte(jsonTypedData), &data); err != nil {
 		return nil, err
 	}
 
-	p := &Permit{
+	p := &StakeAndBakePermit{
 		ChainId: math.HexOrDecimal256(big.Int{}),
 	}
 
@@ -87,7 +86,7 @@ func NewFromJson(jsonTypedData string) (*Permit, error) {
 	return p, nil
 }
 
-func (p *Permit) SetOwnerFromString(val string) error {
+func (p *StakeAndBakePermit) SetOwnerFromString(val string) error {
 	ownerAddr, err := common.NewMixedcaseAddressFromString(val)
 	if err != nil {
 		return err
@@ -97,7 +96,7 @@ func (p *Permit) SetOwnerFromString(val string) error {
 	return nil
 }
 
-func (p *Permit) SetSpenderFromString(val string) error {
+func (p *StakeAndBakePermit) SetSpenderFromString(val string) error {
 	spenderAddr, err := common.NewMixedcaseAddressFromString(val)
 	if err != nil {
 		return err
@@ -107,7 +106,7 @@ func (p *Permit) SetSpenderFromString(val string) error {
 	return nil
 }
 
-func (p *Permit) SetValueFromString(val string) error {
+func (p *StakeAndBakePermit) SetValueFromString(val string) error {
 	value, ok := new(big.Int).SetString(val, 10)
 	if !ok {
 		return errors.New("invalid value string")
@@ -117,7 +116,7 @@ func (p *Permit) SetValueFromString(val string) error {
 	return nil
 }
 
-func (p *Permit) SetNonceFromString(val string) error {
+func (p *StakeAndBakePermit) SetNonceFromString(val string) error {
 	nonce, ok := new(big.Int).SetString(val, 10)
 	if !ok {
 		return errors.New("invalid nonce string")
@@ -127,29 +126,27 @@ func (p *Permit) SetNonceFromString(val string) error {
 	return nil
 }
 
-func (p *Permit) SetDeadlineFromString(val string) error {
+func (p *StakeAndBakePermit) SetDeadlineFromString(val string) error {
 	deadline, err := strconv.ParseInt(val, 10, 64)
 	if err != nil {
 		return err
 	}
 	p.Deadline = deadline
-
 	return nil
 }
 
-func (p Permit) Validate() error {
+func (p StakeAndBakePermit) Validate() error {
 	// TODO: implement more validations
 
 	// make sure expiry time is in the future
 	if p.Deadline < time.Now().Unix() {
 		return errors.New("expiry time is in the past")
 	}
-
 	return nil
 }
 
 // ToTypedData converts the tx to a EIP-712 Typed Data structure for signing
-func (p Permit) ToTypedData() apitypes.TypedData {
+func (p StakeAndBakePermit) ToTypedData() apitypes.TypedData {
 	var domainType = []apitypes.Type{
 		{Name: "verifyingContract", Type: "address"},
 		{Name: "chainId", Type: "uint256"},
