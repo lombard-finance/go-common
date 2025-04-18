211c3435bce1a209f51a3e71d5f5931c59894d25
pmurzin <pmurzin@yandex.ru>
2025-04-18 15:30:26 +0300

    Add solana signature verification (#10)
    * Add solana signature verification
    
    * Add solana const
    
    * Add solana check for address
    
    * Add address normalization
    
    * Remove signature verification
    
    ---------
    
    Co-authored-by: Petr Murzin <petr@lombard.finance>

--- signature/referral.go
+++
@@ -1,7 +0,0 @@
-package signature
-
-const ReferralMessageTemplate = "I have read and agreed to the terms of service: https://docs.lombard.finance/legals/terms-of-service"
-
-func VerifyReferralSignature(signer, signature []byte) error {
-	return VerifySignature(signer, signature, ReferralMessageTemplate)
-}
